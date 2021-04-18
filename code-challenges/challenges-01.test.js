0d83c659aea26c8e7fedbcbcfe25c82421c44e07
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-04-18 21:31:51 +0300

    clang0.01.4

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -89,7 +89,11 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
+
+  for(let i =0 ; i< times ; i++){
+    callback(arr, num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------

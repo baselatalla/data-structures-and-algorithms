beb34234a0ed634ed970dbc7469ca131dd1704ee
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-04-18 21:06:27 +0300

    clang0.01.4

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -60,10 +60,10 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 const greeting = (word) => {
   let array = [];
   word.forEach((iteam) => {
-    array.push(`${iteam.toUpperCase()}!`)
+    array.push(`${iteam.toUpperCase()}!`);
   });
   return array;
-
+};
 const speaker = (words, callback) => {
   let array = [];
   words.forEach((iteam) => {
